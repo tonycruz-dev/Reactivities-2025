bb88ee0dfbb63c04f5b20476ca3cf196a92d7146
Antonio Lopes da Cruz <tonycruz1@hotmail.com>
2025-04-09 18:31:32 +0100

    End of section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ActivitiesController(AppDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+
+        if (activity == null) return NotFound();
+
+        return activity;
+    }
+}
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
