798ff0af1485a7cac6c8c0f5b2a45ec89f79b2fe
Antonio Lopes da Cruz <tonycruz1@hotmail.com>
2025-04-11 16:27:41 +0100

    End of section 8

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,72 +1,24 @@
 
 import { Box, Container, CssBaseline } from "@mui/material";
-//import axios from "axios";
-//import { useState } from "react"
 import NavBar from "./NavBar";
-import { Outlet } from "react-router";
-//import { useQuery } from "@tanstack/react-query";
-//import {useActivities}  from "../../lib/hooks/useActivities";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
 function App() {
-  //const [activities, setActivities] = useState<Activity[]>([]);
-  // const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  // const [editMode, setEditMode] = useState(false);
-  //const {activities, isPending} = useActivities();
-  
-  // const {data: activities, isPending} = useQuery({
-  //   queryKey: ['activities'],
-  //   queryFn: async () => {
-  //     const response = await axios.get<Activity[]>("https://localhost:5001/api/activities")
-  //     return  response.data;
-  //     }
-  // })
-  // useEffect(() => {
-  //  axios.get<Activity[]>("https://localhost:5001/api/activities")
-  //     .then(response => setActivities(response.data))
-  //   // fetch('https://localhost:5001/api/activities')
-  //   //   .then(response => response.json())
-  //   //   .then(data => setActivities(data))
-  // }, [])
-    // const handleSelectActivity = (id: string) => {
-    //   setSelectedActivity(activities!.find((x) => x.id === id));
-    // };
+ const location = useLocation();
 
-    // const handleCancelSelectActivity = () => {
-    //   setSelectedActivity(undefined);
-    // };
-
-    // const handleOpenForm = (id?: string) => {
-    //   if (id) handleSelectActivity(id);
-    //   else handleCancelSelectActivity();
-    //   setEditMode(true);
-    // };
-
-   
-
-    // const handleSubmitForm = (activity: Activity) => {
-    //   // if (activity.id) {
-    //   //   setActivities(
-    //   //     activities.map((x) => (x.id === activity.id ? activity : x))
-    //   //   );
-    //   // } else {
-    //   //   const newActivity = { ...activity, id: activities.length.toString() };
-    //   //   setSelectedActivity(newActivity);
-    //   //   setActivities([...activities, newActivity]);
-    //   // }
-    //   console.log(activity);
-    //   setEditMode(false);
-    // };
-
-    // const handleDelete = (id: string) => {
-    //   //setActivities(activities.filter((x) => x.id !== id));
-    //   console.log(id);
-    // };
   return (
     <Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
       <CssBaseline />
-      <NavBar />
-      <Container maxWidth="xl" sx={{ mt: 3 }}>
-        <Outlet />
-      </Container>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <NavBar />
+          <Container maxWidth="xl" sx={{ mt: 3 }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </Box>
   );
 }
--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import { Grid2, Typography, } from "@mui/material";
 import ActivityList from "./ActivityList";
 
 import { useActivities } from "../../../lib/hooks/useActivities";
+import ActivityFilters from "./ActivityFilters";
 
 
 export default function ActivityDashboard() {
@@ -10,12 +11,12 @@ export default function ActivityDashboard() {
   if(!activities || isPending) return <Typography>Loading...</Typography>;
   return (
     <Grid2 container spacing={3}>
-      <Grid2 size={7}>
+      <Grid2 size={8}>
         <ActivityList
         />
       </Grid2>
-      <Grid2 size={5}>
-       Activity filter go here
+      <Grid2 size={4}>
+       <ActivityFilters />
       </Grid2>
     </Grid2>
   );
